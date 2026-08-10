85f22c455b1f1d260b27dac50dbb41379121091a
Saleh Karimov <salehkrimov961@gmail.com>
2026-08-10 12:21:22 +0400

    Add OOP to Account feature

--- financetracker.py
+++ financetracker.py
@@ -3,65 +3,68 @@ print("\n" + "="*40 + " FINANCE TRACKER " + "="*40)
 import pwinput
 import json
 import requests
-from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
-try:
- account_file = open("account.txt", "r")
- content = account_file.read()
- lines = content.split("\n")
- username = lines[0]
- password = lines [1]
- account_file.close()
- # ---------- LOG IN ----------
- print("\n" + "-"*44 + " LOG IN " + "-"*44)
+class Account:
+   def __init__(self):
+      self.username = ""
+      self.password = ""
+   
+   def create_account(self):
+      print("\n" + "-"*39 + " CREATE AN ACCOUNT " + "-"*39)
+      while True:
+           self.username = input("\nCreate an username: ")
+           if len(self.username) < 5:
+               print("Error: Username is too short (min 5 characters)!")
+           elif len(self.username) > 12:
+               print("Error: Username is too long (max 12 characters)!")
+           else:
+               print("Username set successfully.")
+               break 
+      while True:
+           self.password = pwinput.pwinput(prompt="\nCreate a password: ", mask="*")
+           if len(self.password) < 7:
+               print("Error: Password is too short! (min 7 characters)!")
+           elif len(self.password) > 18:
+               print("Error: Password is too long (max 18 characters)!")
+           else:
+               print("Password set successfully.")
+               break
+   
+   def login(self):
+      print("\n" + "-"*44 + " LOG IN " + "-"*44)
+      max_attempts = 3
+      attempts = 0
+      while attempts < max_attempts:
+           username_login = input("\nUsername: ")
+           password_login = pwinput.pwinput(prompt="Password: ", mask="*")         
+           if self.username == username_login and self.password == password_login:
+               print("Logged in successfully.")
+               break # Exit the login loop
+           else:
+               attempts += 1
+               print(f"Error: Wrong username or password, {max_attempts - attempts} attempts remaining.")   
+      if attempts == max_attempts:
+           print("\n SYSTEM LOCKED: Maximum login attempts exceeded!")
+           exit()
+   
+   def start(self):
+      try:
+       account_file = open("account.txt", "r")
+       content = account_file.read()
+       lines = content.split("\n")
+       self.username = lines[0]
+       self.password = lines [1]
+       account_file.close()
+       self.login()
+      except FileNotFoundError:
+         account_file = open("account.txt", "w")
+         self.create_account()
+         account_file.write(f"{self.username}\n{self.password}") 
+         account_file.close()
 
- max_attempts = 3
- attempts = 0
-
- # Login process for security
- while attempts < max_attempts:
-     username_login = input("\nUsername: ")
-     password_login = pwinput.pwinput(prompt="Password: ", mask="*")
-    
-     if username == username_login and password == password_login:
-         print("Logged in successfully.")
-         break # Exit the login loop
-     else:
-         attempts += 1
-         print(f"Error: Wrong username or password, {max_attempts - attempts} attempts remaining.")
-
- if attempts == max_attempts:
-     print("\n SYSTEM LOCKED: Maximum login attempts exceeded!")
-     exit()
-
-except FileNotFoundError:
- account_file = open("account.txt", "w")
-    # ---------- CREATE ACCOUNT ----------
- print("\n" + "-"*39 + " CREATE AN ACCOUNT " + "-"*39)
-
-  # Username validation
- while True:
-     username = input("\nCreate an username: ")
-     if len(username) < 5:
-         print("Error: Username is too short (min 5 characters)!")
-     elif len(username) > 12:
-         print("Error: Username is too long (max 12 characters)!")
-     else:
-         print("Username set successfully.")
-         break 
-
- # Password validation
- while True:
-     password = pwinput.pwinput(prompt="\nCreate a password: ", mask="*")
-     if len(password) < 7:
-         print("Error: Password is too short! (min 7 characters)!")
-     elif len(password) > 18:
-         print("Error: Password is too long (max 18 characters)!")
-     else:
-         print("Password set successfully.")
-         break
- account_file.write(f"{username}\n{password}") 
- account_file.close()
+account = Account()
+account.start()
 
 # ---------- FINANCE TRACKER ----------
 class FinanceTracker:
@@ -223,7 +226,7 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(data.save_data, 'interval', seconds = 1)
 scheduler.start()
 
-data.load_data(finance.incomes, finance.expenses, finance.total_income_categories, finance.total_expense_categories)
+data.load_data()
 while True:
     print("\n" + "-"*45 + " MENU " + "-"*45)
     print("""1. Add Income
